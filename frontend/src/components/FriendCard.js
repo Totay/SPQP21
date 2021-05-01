341c10fee448e1a262b38744bc59f3d21cf23da9
unknown <dwong1375@gmail.com>
2021-04-30 22:06:56 -0700

    Gave FriendCard css:hover properties to change appearance on mouseover

--- frontend/src/components/FriendCard.js
+++ frontend/src/components/FriendCard.js
@@ -9,10 +9,14 @@ const FriendCard = (props) => {
     const icon = props.icon
     const status = props.status
     const online = props.online
+
+    const onClick = () => {console.log('pressed')}
     
     //need to find way to crop long names to fit within card length
+    //work on way to make FriendCard clickable
+    //also make images work
     return (
-        <div className = {style['friend-card']} onClick = {() => {console.log('pressed')} }>
+        <div className = {style['friend-card']} onClick = {onClick}>
             <div> <img src = {icon} /> </div>
             <div>{name}</div>
             <div>{online ? <BiBrightness color = '#58AA43'/> : <BiMoon color = '#4570DE'/>}</div>
